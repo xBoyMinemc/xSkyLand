4bd199555b109498fa91cb4ff845795a61cef440
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2022-08-07 20:37:01 +0800

    增加指定实体对象查询计分板Participant；  Entity=》ScoreboardIdentity

--- tscripts/lib/xboyTools/scoreBase/rw.ts
+++ tscripts/lib/xboyTools/scoreBase/rw.ts
@@ -1,4 +1,4 @@
-import { Dimension, Scoreboard, ScoreboardIdentity, ScoreboardObjective, world } from "mojang-minecraft";
+import { Dimension, Entity, Scoreboard, ScoreboardIdentity, ScoreboardObjective, world } from "mojang-minecraft";
 
 const overworld : Dimension = world.getDimension("overworld");
 // let xboy;
@@ -16,23 +16,22 @@ const overworld : Dimension = world.getDimension("overworld");
 
 const GetScoreBoard    : Scoreboard = world.scoreboard;
 const GetScoreObject    = (...args: string[]) : ScoreboardObjective|ScoreboardObjective[] =>{return  args.length ? GetScoreBoard.getObjective(args[0])    : GetScoreBoard.getObjectives()};
-const GetScorePartic    = (...args: { getParticipants: () => ScoreboardIdentity[]; }[]) =>{return  args.length ? args[0].getParticipants() : GetScoreBoard.getParticipants()};
+const GetScorePartic    = (args: Entity & ScoreboardObjective ) : Entity|ScoreboardIdentity[]=>{return  args ? (args.dimension?Array.from(GetScoreBoard.getParticipants()).find(Participant=>Participant.getEntity()==args):args[0].getParticipants()) : GetScoreBoard.getParticipants()};
 // @ts-ignore
 const GetScorePoints    = (object : ScoreboardObjective|string,partic: string) : number => {return  Array.from(((typeof object == "string" ) ? GetScoreObject(object) : object).getScores()).find((_)=>_.participant.displayName == partic).score}
 
-
 // @ts-ignore
-const AssScoreObject    = (ObjName: string) : ScoreboardObjective=>{return  GetScoreObject().find((scoreboard : { id: string; })=>{if(scoreboard .id === ObjName)return true})};
+const AssScoreObject    = (ObjName: string) : ScoreboardObjective|undefined=>{return  GetScoreObject().find((scoreboard : { id: string; })=>{if(scoreboard .id === ObjName)return true})};
 // @ts-ignore
 const AssScorePartic    = (...args: any[])=>{return args.length === 2 ? args[1].getParticipants().find((participant: { displayName: string; })=>{if(participant.displayName === args[0])return true}) : GetScorePartic().find((participant: { displayName: string; })=>{if(participant.displayName === args[0])return true})};
 
 
 ///scoreboard objectives remove testObjectName
 ///scoreboard objectives add testObjectName dummy ss
-const DelScoreObjectAsync    = (ObjName: string | ScoreObject)=>{return overworld.runCommandAsync(`scoreboard objectives remove ${(typeof ObjName === typeof "xBoyMinemc") ? ObjName : ObjName.id}`)};
+const DelScoreObjectAsync    = (ObjName: string | ScoreboardObjective)=>{return overworld.runCommandAsync(`scoreboard objectives remove ${(typeof ObjName === typeof "xBoyMinemc") ? ObjName : ObjName.id}`)};
 const NewScoreObjectAsync    = (...args: string[])=>{return overworld.runCommandAsync(`scoreboard objectives add ${args[0]} ${args[2]||"dummy"} ${args[1]}`)};
 
-const DelScoreObject    = (ObjName: string | ScoreObject)=>{overworld.runCommand(`scoreboard objectives remove ${(typeof ObjName === typeof "xBoyMinemc") ? ObjName : ObjName.id}`)};
+const DelScoreObject    = (ObjName: string | ScoreboardObjective)=>{overworld.runCommand(`scoreboard objectives remove ${(typeof ObjName === typeof "xBoyMinemc") ? ObjName : ObjName.id}`)};
 const NewScoreObject    = (...args: string[])=>{overworld.runCommand(`scoreboard objectives add ${args[0]} ${args[2]||"dummy"} ${args[1]}`)};
 ///scoreboard objectives setdisplay list ScoreName ascending
 const DisScoreObject    = (...args: string[])=>{overworld.runCommand(`scoreboard objectives setdisplay ${(typeof args[0] === typeof 520) ? ['list','sidebar','belowname'][args[0]] : args[0]} ${((typeof args[1] === typeof "云梦") ? args[1] : args[1].id)+(args[2]?(" "+(typeof args[2] === "string" ? args[2] : ['ascending','descending'][args[2]])):"")}`)};
@@ -40,7 +39,7 @@ const DisScoreObject    = (...args: string[])=>{overworld.runCommand(`scoreboard
 
 
 ///scoreboard players add "Xboy minemc" testObjectName 3
-const AddScorePointsAsync    = (...args: string[])=>{overworld.runCommandAsync(`scoreboard players add ${args[0].name ? ('"'+ args[0].name +'"') : (args[0].includes('"') ? args[0] : ('"'+args[0]+'"'))} ${(typeof args[1] === typeof "Xboy minemc")?args[1]:('"'+args[1].id+'"')} ${args[2]}`)};
+const AddScorePointsAsync    = (...args: (string & Entity)[])=>{overworld.runCommandAsync(`scoreboard players add ${args[0].name ? ('"'+ args[0].name +'"') : (args[0].includes('"') ? args[0] : ('"'+args[0]+'"'))} ${(typeof args[1] === typeof "Xboy minemc")?args[1]:('"'+args[1].id+'"')} ${args[2]}`)};
 const SetScorePointsAsync    = (...args: string[])=>{overworld.runCommandAsync(`scoreboard players set ${args[0].name ? ('"'+ args[0].name +'"') : (args[0].includes('"') ? args[0] : ('"'+args[0]+'"'))} ${(typeof args[1] === typeof "Xboy minemc")?args[1]:('"'+args[1].id+'"')} ${args[2]}`)};
 
 const AddScorePoints    = (...args: string[])=>{overworld.runCommand(`scoreboard players add ${args[0].name ? ('"'+ args[0].name +'"') : (args[0].includes('"') ? args[0] : ('"'+args[0]+'"'))} ${(typeof args[1] === typeof "Xboy minemc")?args[1]:('"'+args[1].id+'"')} ${args[2]}`)};
