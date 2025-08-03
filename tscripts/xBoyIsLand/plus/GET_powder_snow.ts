e7430b137684c2cdd87b2181e233dbed1d0cad5c
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2025-08-03 20:42:33 +0800

    now is now

--- tscripts/xBoyIsLand/plus/GET_powder_snow.ts
+++ tscripts/xBoyIsLand/plus/GET_powder_snow.ts
@@ -2,7 +2,7 @@
 
 import { world, system, ItemStack } from '@minecraft/server'
 
-world.afterEvents.playerInteractWithBlock.subscribe(event => {
+world.beforeEvents.playerInteractWithBlock.subscribe(event => {
     const { player, itemStack: item } = event
     if (player === undefined || player.location === undefined) return
     if (item === undefined || item.typeId === undefined) return
@@ -16,18 +16,28 @@ world.afterEvents.playerInteractWithBlock.subscribe(event => {
     if (nownow.typeId !== 'minecraft:bucket') return "我判断了两次物品，一次判断是否是桶，一次也判断是否是桶"
     if (nownow.amount <= 0) return
 
-    container.addItem(new ItemStack('minecraft:powder_snow_bucket', 1))
+    system.run(() => {
 
-    if (nownow.amount === 1) {
-        container.setItem(player.selectedSlotIndex, new ItemStack('minecraft:air', 33))
-    } else if (nownow.amount > 1) {
-        nownow.amount -= 1
-        container.setItem(player.selectedSlotIndex, nownow)
-    }
-    player.playSound('bucket.fill_powder_snow', {
-        location: block.location,
-        volume: 1,
-        pitch: 1
+        const container = player.getComponent('minecraft:inventory').container
+        const nownow = container.getItem(player.selectedSlotIndex);
+
+        if (nownow.typeId !== 'minecraft:bucket') return "我判断了两次物品，一次判断是否是桶，一次也判断是否是桶"
+        if (nownow.amount <= 0) return
+        block.setType('minecraft:air')
+
+        container.addItem(new ItemStack('minecraft:powder_snow_bucket', 1))
+
+        if (nownow.amount === 1) {
+            container.setItem(player.selectedSlotIndex, new ItemStack('minecraft:air', 33))
+        } else if (nownow.amount > 1) {
+            nownow.amount -= 1
+            container.setItem(player.selectedSlotIndex, nownow)
+        }
+        player.playSound('bucket.fill_powder_snow', {
+            location: block.location,
+            volume: 1,
+            pitch: 1
+        })
     })
 
 })
